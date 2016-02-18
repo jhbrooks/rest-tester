00cffa297ed643e17ead7178dcc35ca75e01a7e2
James Brooks <jhbrooks@uchicago.edu>
2016-02-18 18:03:27 -0500

    Set up script to perform the 4 GET REST requests

--- app/scripts/rest_requestor.rb
+++ app/scripts/rest_requestor.rb
@@ -1,5 +1,15 @@
 #!/usr/bin/env ruby
 
 require "rest-client"
-url = "http://localhost:3000/users"
-puts RestClient.get(url)
+
+index_url = "http://localhost:3000/users"
+puts RestClient.get(index_url)
+
+show_url = "http://localhost:3000/users/1"
+puts RestClient.get(show_url)
+
+new_url = "http://localhost:3000/users/new"
+puts RestClient.get(new_url)
+
+edit_url = "http://localhost:3000/users/1/edit"
+puts RestClient.get(edit_url)
